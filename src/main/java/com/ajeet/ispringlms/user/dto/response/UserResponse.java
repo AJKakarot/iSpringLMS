ff72099c5bc175f0085dd244dac1cc419e8a0800
Ajeet Gupta <gajeet031@gmail.com>
2026-07-06 04:19:16 +0530

    Contains user details required during registration.

--- src/main/java/com/ajeet/ispringlms/user/dto/response/UserResponse.java
+++ src/main/java/com/ajeet/ispringlms/user/dto/response/UserResponse.java
@@ -1,12 +1,12 @@
 package com.ajeet.ispringlms.user.dto.response;
 
 import com.ajeet.ispringlms.user.enums.Role;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
+/**
+ * Response DTO for user information.
+ */
 @Getter
 @Builder
 public class UserResponse {
@@ -19,19 +19,4 @@ public class UserResponse {
 
     private Role role;
 
-    @Getter
-    @Setter
-    public static class UserRegistrationRequest {
-
-        @NotBlank(message = "Name is required")
-        private String name;
-
-        @Email(message = "Invalid email")
-        @NotBlank(message = "Email is required")
-        private String email;
-
-        @NotBlank(message = "Password is required")
-        private String password;
-
-    }
 }
